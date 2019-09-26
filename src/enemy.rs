b7879caec39390b1feb334dc99cfd0c18e57d5fa
Zach Stone <zach@zerovolts.com>
2019-09-25 22:28:35 -0400

    Add basic UI with coin count

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,27 +1,42 @@
 use amethyst::{
     assets::Handle,
     core::{math::Vector3, transform::Transform},
-    ecs::prelude::{Component, DenseVecStorage, Entities, Join, System, WriteStorage},
+    ecs::prelude::{
+        Component, DenseVecStorage, Entities, Join, ReadExpect, System, Write, WriteStorage,
+    },
     prelude::*,
     renderer::{SpriteRender, SpriteSheet},
+    ui::UiText,
 };
 
 use crate::velocity::Velocity;
+use crate::{BuildingMaterials, GameUi};
 
 #[derive(Component, Default)]
 pub struct Enemy {
     pub health: i32,
+    pub value: i32,
 }
 
 pub struct EnemySystem;
 
 impl<'s> System<'s> for EnemySystem {
-    type SystemData = (Entities<'s>, WriteStorage<'s, Enemy>);
+    type SystemData = (
+        Entities<'s>,
+        WriteStorage<'s, Enemy>,
+        Write<'s, BuildingMaterials>,
+        WriteStorage<'s, UiText>,
+        ReadExpect<'s, GameUi>,
+    );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (entities, mut enemies) = data;
+        let (entities, mut enemies, mut building_materials, mut ui_text, game_ui) = data;
         for (entity, enemy) in (&entities, &mut enemies).join() {
             if enemy.health <= 0 {
+                building_materials.coins += enemy.value;
+                if let Some(text) = ui_text.get_mut(game_ui.coin_display) {
+                    text.text = format!("Coins: {}", building_materials.coins.to_string());
+                }
                 let _ = entities.delete(entity);
             }
         }
@@ -39,7 +54,10 @@ pub fn create_enemy(world: &mut World, sprite_sheet: Handle<SpriteSheet>, origin
 
     let velocity = Velocity::new(Vector3::new(0.25, 0.0, 0.0));
 
-    let enemy = Enemy { health: 100 };
+    let enemy = Enemy {
+        health: 100,
+        value: 10,
+    };
 
     world
         .create_entity()
--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,20 @@ mod tower;
 mod velocity;
 
 use amethyst::{
+    assets::{AssetStorage, Loader},
     core::{
+        ecs::Entity,
         math::Vector3,
         transform::{Transform, TransformBundle},
     },
+    input::{InputBundle, StringBindings},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         Camera, RenderingBundle,
     },
+    ui::{get_default_font, Anchor, FontAsset, RenderUi, UiBundle, UiImage, UiText, UiTransform},
     utils::application_root_dir,
 };
 
@@ -52,6 +56,7 @@ impl SimpleState for GameplayState {
         }
 
         world.add_resource(sprite_sheet_map);
+        init_ui(world);
         init_camera(world);
     }
 }
@@ -64,16 +69,21 @@ fn main() -> amethyst::Result<()> {
     let config_dir = app_root.join("config");
     let display_config_path = config_dir.join("display.ron");
 
+    let rendering_bundle = RenderingBundle::<DefaultBackend>::new()
+        .with_plugin(RenderFlat2D::default())
+        .with_plugin(RenderUi::default())
+        .with_plugin(
+            RenderToWindow::from_config_path(display_config_path)
+                .with_clear([0.34, 0.36, 0.52, 1.0]),
+        );
+
+    let input_bundle = InputBundle::<StringBindings>::new();
+
     let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderFlat2D::default()),
-        )?
+        .with_bundle(rendering_bundle)?
         .with_bundle(TransformBundle::new())?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(input_bundle)?
         .with(VelocitySystem, "velocity_system", &[])
         .with(TowerSystem, "tower_system", &[])
         .with(EnemySystem, "enemy_system", &[])
@@ -97,4 +107,50 @@ fn init_camera(world: &mut World) {
         .with(Camera::standard_2d(SCREEN_WIDTH, SCREEN_HEIGHT))
         .with(transform)
         .build();
+}
+
+#[derive(Default)]
+pub struct BuildingMaterials {
+    coins: i32,
+}
+
+pub struct GameUi {
+    pub coin_display: Entity,
+}
+
+fn init_ui(world: &mut World) {
+    let font_handle = {
+        let loader = world.read_resource::<Loader>();
+        let font_store = world.read_resource::<AssetStorage<FontAsset>>();
+        get_default_font(&loader, &font_store)
+    };
+
+    let coin_display_transform = UiTransform::new(
+        "coin_display_transform".to_string(),
+        Anchor::TopLeft,
+        Anchor::TopLeft,
+        0.,
+        0.,
+        1.,
+        200.,
+        50.,
+    );
+
+    let coin_display_text = UiText::new(
+        font_handle.clone(),
+        "Coins: 0".to_string(),
+        [1., 1., 1., 1.],
+        20.,
+    );
+
+    let coin_display_background = UiImage::SolidColor([0.2, 0.2, 0.2, 0.5]);
+
+    let coin_display = world
+        .create_entity()
+        .with(coin_display_transform)
+        .with(coin_display_text)
+        .with(coin_display_background)
+        .build();
+
+    world.add_resource(GameUi { coin_display });
 }
